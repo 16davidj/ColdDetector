7f32861643e33b7404f92ae2858f261adfffa744
David <davidjin24@gmail.com>
2015-06-20 14:09:38 -0700

    still cancel alarm failing

--- src/com/example/davidfirstapp/Alarm.java
+++ src/com/example/davidfirstapp/Alarm.java
@@ -35,7 +35,6 @@ public class Alarm extends BroadcastReceiver
      Intent i = new Intent(c, Alarm.class);
      PendingIntent pi = PendingIntent.getBroadcast(c, 0, i, 0);
      am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), pi); // Millisec * Second * Minute
-     am.cancel(pi);
  }
  public void cancelAlarm(Context c)
  {
